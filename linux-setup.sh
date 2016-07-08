169477c7c77fc38734f925baebb72a1c5ea46c97
Andrew Macdonald <andrew@andrewmac.me>
2016-07-08 01:32:29 +0100

    Improved package sources, installs, and order

--- linux-setup.sh
+++ linux-setup.sh
@@ -13,22 +13,23 @@ mount /mnt/dmsPublicFiles
 echo 'Acquire::http::proxy "http://dms-server.local:3142";' >> /etc/apt/apt.conf.d/00proxy
 echo 'Acquire::https::proxy "DIRECT";' >> /etc/apt/apt.conf.d/00proxy
 
-# Add PPAs { 
+# curl
+apt-get install curl
+
+# Add PPAs {
   
-  #
-  # chrome
-  # 
+  # Google Chrome
+  curl --silent https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
   echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.chrome.list
-  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 
-  # 
-  # node v0.12
-  # 
-  wget -q -O - https://deb.nodesource.com/setup_0.12 | bash -
+  # node.js v0.12
+  curl --silent https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+  NODE_VERSION=node_0.12
+  DISTRO="$(lsb_release -s -c)"
+  echo "deb https://deb.nodesource.com/$NODE_VERSION $DISTRO main" >> /etc/apt/sources.list.d/nodesource.list
+  echo "deb-src https://deb.nodesource.com/$NODE_VERSION $DISTRO main" >> /etc/apt/sources.list.d/nodesource.list
 
-  # 
-  # Atom editor
-  # 
+  # Atom editor from unofficial PPA
   add-apt-repository ppa:webupd8team/atom
 
 # }
@@ -39,98 +40,77 @@ apt-get update
 # Mostly borrowed from https://github.com/jfhbrook/makerspace-linux/blob/master/chroot-commands.sh
 # Install packages {
 
-  #
   # Basic support for compiling stuff
-  #
-  apt-get install -y build-essential curl
+  apt-get install -y build-essential
+  
+  # Source control tools
+  apt-get install -y bzr git mercurial svn
+  
+  # Web browser
+  apt-get install -y google-chrome-stable
 
-  #
   # Missing scripting languages/environments
-  #
   apt-get install -y php5 nodejs
 
-  #
   # Missing package management tools
-  #
   apt-get install -y php-pear pip rubygems
-  curl -sS https://getcomposer.org/installer | php
+  # Composer funky install script because using normal package managers is too sensible for PHP developers...
+  COMPOSER_EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  COMPOSER_ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+  if [ "$COMPOSER_EXPECTED_SIGNATURE" = "$COMPOSER_ACTUAL_SIGNATURE" ]
+  then
+    php composer-setup.php --quiet
+    rm composer-setup.php
+  else
+    >&2 echo 'ERROR: Invalid composer installer signature'
+    rm composer-setup.php
+  fi
   mv composer.phar /usr/local/bin/composer
 
-  #
-  # Source control tools
-  #
-  apt-get install -y bzr git mercurial svn
-
-  #
-  # 2d graphics tools
-  #
+  # 2D graphics tools
   apt-get install -y gimp inkscape
 
-  #
   # CAD software
-  #
   apt-get install -y blender freecad openscad wings3d
 
-  #
   # STL analysis/repair/etc tools
-  #
   apt-get install -y assimp-utils admesh meshlab
 
-  #
   # RepRap toolchain
-  #
   # TODO: slic3r, printrun, tools on thingiverse
   # TODO: thingiverse cli? (npm view thingiverse)
   apt-get install -y skeinforge
 
-  #
   # Arduino and AVR toolchain
-  #
   apt-get install -y gcc-avr arduino arduino-mk avrdude avr-libc
 
-  #
   # Text/code editing
-  #
   apt-get install -y vim okteta atom
 
-  #
   # Networking client/diagnostic tools
-  #
-  apt-get install -y atftp elinks iptables netcat6 nslookup pure-ftpd tcpdump telnet-bsd whois wireshark
+  apt-get install -y atftp elinks iptables netcat6 nslookup lftp pure-ftpd tcpdump telnet-bsd whois wireshark
 
-  #
   # Networking server tools
-  #
   # TODO: How do these behave on a livecd??
   # TODO: samba xinetd mysql??
   # TODO: ecstatic? (npm view ecstatic)
   apt-get install -y apache2 libapache2-mod-perl2 libapache2-mod-php5 sshd puppet
 
-  #
   # Hardware tools
   # TODO: ntpasswd
   apt-get install -y ddrescue gparted 
 
-  #
   # Audio/Video
-  #
   apt-get install -y audacity avidemux openshot qtractor sox vlc
 
-  #
   # Office Stuff
-  #
   apt-get remove -y sylpheed pidgin xpad
-  
-  #
-  # Interwebs
-  #
-  apt-get install -y google-chrome-stable
 
 # }
 
 # Install hardware {
   
-  #
   # Epson Stylus SX215 printer
   # Using gdebi in non-interactive mode instead of dpkg because it will resolve dependencies from official packages
   gdebi -n /mnt/dmSharedStorage/Shared/Epson\ Stylus\ SX210/Linux/epson-inkjet-printer-escpr_1.6.1-1lsb3.2_amd64.deb
@@ -139,6 +119,9 @@ apt-get update
   gdebi -n /mnt/dmSharedStorage/Shared/Epson\ Stylus\ SX210/Linux/iscan-bundle-1.0.0.x64.deb/core/iscan_2.30.1-1~usb0.1.ltdl7_amd64.deb
   gdebi -n /mnt/dmSharedStorage/Shared/Epson\ Stylus\ SX210/Linux/iscan-bundle-1.0.0.x64.deb/plugins/iscan-network-nt_1.1.1-1_amd64.deb
   apt-get install sane sane-utils libsane-extras xsane
+  # TODO: Install the printer itself instead of just the drivers
+  
+  # TODO: HP Photosmart 5510 over the network
 
 # }
 
